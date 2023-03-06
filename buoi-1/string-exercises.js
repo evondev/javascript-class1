0bc0c8d894372d32d848aa947b947f50b307686d
evondev <trananhtuan400@gmail.com>
2023-03-07 00:01:24 +0700

    Add string exercise

---
+++ buoi-1/string-exercises.js
@@ -0,0 +1,12 @@
+// 1. Convert string to dash string
+const str = "I love to learn Javascript";
+// expected: "i-love-to-learn-javascript"
+// 2. Truncate string to 30 characters with ...
+const longStr = `Write a JavaScript function to split a string and convert it into an array of words`;
+// expected: Write a JavaScript function to ...
+// 3.
+const newStr = `javascript is awesome`;
+// expected: Javascript is awesome
+// 4.
+const lastStr = "evondev is awesome";
+// expected: awesome is evondev
